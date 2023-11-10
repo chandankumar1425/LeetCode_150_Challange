14b9e07aa08daac496e7d3c58c16a3b6250cbcc0
Chandan Kumar Behera <chandankumarbehera1425@gmail.com>
2023-11-10 23:11:02 +0530

    Sqrt(x)

---
+++ Sqrt(x).js
@@ -0,0 +1,35 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+var mySqrt = function(x) {
+    var beg = 0, end = x, ans = 0;
+    
+    while(beg <= end) {
+        var mid = Math.floor((beg + end)/2);
+        
+        if(mid*mid > x) {
+            end = mid - 1;
+        }
+         
+        else { // mid*mid <= x
+            ans = mid;
+            beg = mid + 1;
+        }
+    }
+    return ans;
+};
